b74d6b8f24da34b6a43a29c152ba281329072501
yvanol <rikamyvanol@gmail.com>
2025-08-29 16:05:05 +0100

    server config for frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
- export const server = "http://localhost:8000/api/v2";
+ export const server = "https://store-liard-omega.vercel.app/api/v2";
  
- export const backend_url = "http://localhost:8000/";
+ export const backend_url = "https://store-liard-omega.vercel.app/";
