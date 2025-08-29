542e5a2a7cbc826ecc8f9809ec86796dfbc3cd9a
yvanol <rikamyvanol@gmail.com>
2025-08-29 15:11:27 +0100

    config v3

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
