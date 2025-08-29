579a9c98d16acd690632d17c1cfd4758f2d8598f
yvanol <rikamyvanol@gmail.com>
2025-08-29 16:12:13 +0100

    frondend link fix

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://store-tajg.vercel.app/",
     credentials: true,
   })
 );
