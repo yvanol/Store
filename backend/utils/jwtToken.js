bd214a496d39193316c8a0ff250841ea0d61a5fc
yvanol <rikamyvanol@gmail.com>
2025-08-29 16:58:38 +0100

    version6.5

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,6 +7,8 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSites: "none",
+    secure: "true"
   };
 
   res.status(statusCode).cookie("token", token, options).json({
