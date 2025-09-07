e849d2bb367e0eea8f36821620e51f687d3225f2
Rikam Yvanol Giovani <85713319+yvanol@users.noreply.github.com>
2025-09-07 16:16:37 +0000

    UPDATE 1 SERVER SOCKET

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import styles from "../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://socket-server-bxel.onrender.com";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
